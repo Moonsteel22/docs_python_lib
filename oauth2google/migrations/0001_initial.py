5a71bd380e3843971847108c5cf674c4e4ec5bd3
aibulat.ryic <aibulat.ryic@gmail.com>
2022-03-04 10:12:59 +0300

    feature: implemented oauth2

---
+++ oauth2google/migrations/0001_initial.py
@@ -0,0 +1,43 @@
+import json
+
+from allauth.socialaccount.models import SocialApp
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.db import migrations
+from django.db import transaction
+from dotenv import find_dotenv
+from dotenv import load_dotenv
+
+PROVIDER_ID = "google"
+APP_NAME = "google-oauth2"
+SITE_DOMAIN = "example.com"
+SITE_NAME = "example"
+
+load_dotenv(find_dotenv())
+
+
+def set_application(apps, schema_editor):
+    with open(settings.CREDENTIALS_AUTH) as j:
+        creds = json.load(j)["web"]
+
+    with transaction.atomic():
+        app = SocialApp(
+            provider=PROVIDER_ID,
+            name=APP_NAME,
+            secret=creds["client_secret"],
+            client_id=creds["client_id"],
+        )
+        app.save()
+        site = Site(domain=SITE_DOMAIN, name=SITE_NAME)
+        site.save()
+        app = SocialApp.objects.get(name=APP_NAME)
+        app.sites.add(site)
+        app.save()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("socialaccount", "__latest__"),
+    ]
+
+    operations = [migrations.RunPython(set_application)]
