417380d697f7d4210532b970b18b9a1a900eb0ee
aibulat.ryic <aibulat.ryic@gmail.com>
2022-03-08 14:49:11 +0300

    feature: added some methods

--- src/services/handlers/document_handler.py
+++ src/services/handlers/document_handler.py
@@ -1,6 +1,13 @@
 from docs_python_lib.src.services.functions import generate_request
 from docs_python_lib.src.services.google_api_auth_service import GoogleApiServices
 from docs_python_lib.src.services.types.base import DocumentRequest
+from docs_python_lib.src.services.types.classes import Location
+from docs_python_lib.src.services.types.classes import Range
+from docs_python_lib.src.services.types.classes import TableCellLocation
+from docs_python_lib.src.services.types.classes import TextStyle
+from docs_python_lib.src.services.types.requests import InsertTableRow
+from docs_python_lib.src.services.types.requests import InsertText
+from docs_python_lib.src.services.types.requests import UpdateTextStyle
 
 
 class DocumentHandler:
@@ -27,3 +34,34 @@ class DocumentHandler:
             documentId=self.documentId,
             body={"requests": [generate_request(request) for request in requests]},
         ).execute()
+
+    def insert_text(
+        self,
+        text: str,
+        location: Location,
+        text_style: TextStyle = None,
+    ) -> tuple[int, list[DocumentRequest]]:
+        end_index = location.index + len(text)
+        requests = [InsertText(text=text, location=location)]
+        if text_style:
+            requests.append(
+                UpdateTextStyle(
+                    text_style=text_style,
+                    range=Range(
+                        start_index=location.index,
+                        end_index=end_index,
+                        segment_id="",
+                    ),
+                ),
+            )
+        return end_index, requests
+
+    def refactored_insert_table_row(
+        self,
+        columns: int,
+        table_cell_location: TableCellLocation,
+    ) -> tuple[int, list[DocumentRequest]]:
+
+        return 1 + 2 * columns, [
+            InsertTableRow(table_cell_location=table_cell_location, insert_below=True),
+        ]
