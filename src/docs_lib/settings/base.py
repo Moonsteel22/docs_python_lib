400f43269bb835425ea3b5eb2dcfecf4839af126
aibulat.ryic <aibulat.ryic@gmail.com>
2022-03-08 14:18:31 +0300

    refactor: refactored settings

--- src/docs_lib/settings/base.py
+++ src/docs_lib/settings/base.py
@@ -15,9 +15,10 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,12 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "123456")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", True))
-
-ALLOWED_HOSTS = ["*"]
-
 
 # Application definition
 
@@ -72,17 +67,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "docs_lib.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-}
 
 
 # Password validation
