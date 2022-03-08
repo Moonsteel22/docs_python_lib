d3b7ad28bdfbb3dc4b5855eca35fc31f7eef58d5
aibulat.ryic <aibulat.ryic@gmail.com>
2022-03-08 12:44:38 +0300

    update: added python-dotenv lib

--- src/docs_lib/settings.py
+++ src/docs_lib/settings.py
@@ -12,11 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-from dotenv import find_dotenv
 from dotenv import load_dotenv
 
 
-load_dotenv(find_dotenv())
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
